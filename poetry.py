a9dfd77ce41be292d8f7418913fb83680f068f43
Chudier <chudierp@gmail.com>
2019-11-08 19:55:21 -0800

    Completed a stetch challenge

--- poetry.py
+++ poetry.py
@@ -11,6 +11,31 @@ Come in!"""
 
 string_list = poem.split("\n")
 
+
+def selection():
+    '''This function allows you to select how you want the poem arranged.'''
+    
+    print("choose how you want the poem arranged:")
+    print("(1) Normal\n(2) Backwards\n(3) Random\n(4) Rearranged")
+
+    while True:
+        selection = input("Number: ")
+        if selection == "1":
+            print(poem)
+            break
+        elif selection == "2":
+            lines_printed_backwards(string_list)
+            break
+        elif selection == "3":
+            lines_printed_random(string_list)
+            break
+        elif selection == "4":
+            rearrange_poem(string_list)
+            break
+        else:
+            print("Invalid selection. Please try again.")
+
+
 def lines_printed_backwards(string_list): 
     """This function takes in a list of strings containing the lines
     of your poem as arguments and will print the poem lines out in reverse
@@ -46,10 +71,22 @@ def rearrange_poem(string_list):
     for string in range(len(string_list)):
         print("!".join(string_list[string]))
 
+def play_again():
+    guess=(input('Play Again?(Y/N'))
+    guess = guess.lower()
+    if guess == 'y':
+        return True
+    else:
+        return False
+    
+
     
       
     
+selection()
 
-lines_printed_backwards(string_list)
-lines_printed_random(string_list)
-rearrange_poem(string_list)
+while play_again():
+    selection()
+# lines_printed_backwards(string_list)
+# lines_printed_random(string_list)
+# rearrange_poem(string_list)
